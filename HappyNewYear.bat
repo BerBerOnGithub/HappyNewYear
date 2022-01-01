88bfb82e266c57a387b200506fdac428a6d6d3f7
BerBerOnGithub <86917016+BerBerOnGithub@users.noreply.github.com>
2022-01-01 15:30:53 +0100

    Add files via upload

---
+++ HappyNewYear.bat
@@ -0,0 +1,20 @@
+@echo off
+title Happy New Year!              - BerBerOnGithub
+echo                                                      Happy New Year
+echo ________________________________________________________________________________________________________________________
+:color
+color 0c
+timeout 1 /nobreak >nul
+color 0e
+timeout 1 /nobreak >nul
+color 0a
+timeout 1 /nobreak >nul
+color 09
+timeout 1 /nobreak >nul
+color 0d
+timeout 1 /nobreak >nul
+color 07
+timeout 1 /nobreak >nul
+color 70
+timeout 1 /nobreak >nul
+goto color
